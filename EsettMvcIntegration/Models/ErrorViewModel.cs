6630f4757ec4df45247d9d5fff406191cf935061
Armando <arodrigues2010@hotmail.com>
2024-08-28 13:05:13 +0200

    create proyect

---
+++ EsettMvcIntegration/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EsettMvcIntegration.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
